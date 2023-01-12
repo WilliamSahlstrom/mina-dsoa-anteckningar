4793efc53322e66bfaf6de24488b5903d659e9c1
William Sahlström <sahlstrw@arcada.fi>
2023-01-12 10:36:39 +0200

    Commit före vi går igenom OOP polymorfism

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,8 +2,27 @@ package org.example;
 
 public class Car extends Vehicle {
 
+    private double price;
+    private double discount = 0.9;
+
+    private String salesPersonEmail;
     public Car(String name) {
         super(name);
+        this.price = 10000;
     }
 
+    public double getPrice() {
+        return price*discount;
+    }
+
+    public String getSalesPersonEmail() {
+        return salesPersonEmail;
+    }
+
+    public void setSalesPersonEmail(String salesPersonEmail) {
+        if(salesPersonEmail.contains("g")) {
+            //Validera email addressen och kasta error om den inte är valid
+        }
+        this.salesPersonEmail = salesPersonEmail;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,14 @@ public class Main {
 
         // Vi instansirar två Car-objekt
         Car car = new Car("VW");
+        car.setSalesPersonEmail("Bossman@gmail.com");
         Bike myBike = new Bike("Crecent");
 
         System.out.println(car.getName());
         System.out.println(myBike.getName());
+
+
+        System.out.printf("%s kostar %.2f, kontakta %s", car.getName(), car.getPrice(), car.getSalesPersonEmail());
 
 
     }
